b776800f419981619602dedd5796676cbf00d124
Agneev Mukherjee <19761269+agneevX@users.noreply.github.com>
2021-03-20 16:53:42 +0530

    Update local_offload.sh

--- scripts/local_offload.sh
+++ scripts/local_offload.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
-set -e
+# shellcheck disable=SC2001
 if [[ $(pidof -x "$(basename "$0")" -o %PPID) ]]; then
 echo "Already running, exiting..."; exit 1; fi
 now="$(date +'%d%m%S')"
-
-if [[ $1 == '-t' ]] || [[ $1 == '--test' ]]; then
-  echo "Test - drive_offload.sh."; exit 1; fi
+disk_usage="$(df --output=pcent / | awk -F '%' 'NR==2{print $1}')"
+disk_info=$(echo "---$disk_usage% SSD USAGE---" && printf "\n\n")
+OPTIMIZE_FLAGS=(--drive-chunk-size 64M --delete-empty-src-dirs --fast-list -v --stats-one-line --stats=5h)
+RCLONE_FLAGS=(--max-transfer=60G --order-by "modtime,ascending" --cutoff-mode=soft --transfers=1 --filter '- local/**' --filter '- .*' --exclude-from=/tmp/lo_exclude_"$now")
 
 if [[ $1 == '-f' ]] || [[ $1 == '--force' ]]; then
-  echo "force flag found... moving all files..."; MIN_AGE=""
+  printf " + Force flag found, moving all files\n"
+  MIN_AGE=""
+elif [[ $disk_usage -gt '90' ]]; then
+  MIN_AGE="--min-age=3d"
+elif [[ $disk_usage -gt '80' ]] && [[ $disk_usage -lt '90' ]]; then
+  MIN_AGE="--min-age=5d"
 else MIN_AGE="--min-age=7d"; fi
 
-HC_URL=https://hc-ping.com/
+if [[ $1 == '-fb' ]] || [[ $1 == '--force-bandwidth' ]]; then
+  printf " + Not enabling bandwidth limiter...\n"; BW_LIMIT=""
+else BW_LIMIT="--bwlimit=6M"; fi
 
+HC_URL=https://hc-ping.com/
 curl -fsS --retry 5 $HC_URL/start
+
 find /opt/.drive/ -type f -links +1 -printf "/%P\n" > /tmp/lo_exclude_"$now"
-o=$(rclone move /opt/.drive --bwlimit=6M --transfers=1 crypt: "$MIN_AGE" --exclude local/** --filter '- .*' --exclude-from=/tmp/lo_exclude_"$now" --order-by "size,mixed,25" --drive-chunk-size 64M -v --stats-one-line --stats=15m --delete-empty-src-dirs --fast-list 2>&1)
-curl -fsS -m 10 --retry 5 --data-raw "$o" "$HC_URL"/$?
+
+o=$(echo "$disk_info" && rclone move "$MIN_AGE" "$BW_LIMIT" /opt/.drive crypt: "${RCLONE_FLAGS[@]}" "${OPTIMIZE_FLAGS[@]}" 2>&1)
+exit_status=$?
+o="$(echo "$o" | sed 's/\&/%26/g')" && o="$(echo "$o" | sed 's/\@/%40/g')"
+
+if [[ $exit_status == '8' ]]; then exit_status='0'; fi
+curl -fsS -m 10 --retry 5 --data-raw "$o" $HC_URL/$exit_status
